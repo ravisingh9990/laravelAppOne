b2c401db5527fe58bc3f9abda12d769a8f347c92
Ravi Singh <ravisingh9990@gmail.com>
2024-07-02 21:10:07 +0530

    factory seeder and pagination added

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,9 +1,26 @@
 <x-layout>
-    @auth
-    <h1>Your are Logged In</h1>
-    @endauth
+    <h1 class="title">Latest Posts</h1>
+    <div grid grid-cols-2 gap-6>
+    @foreach ($posts as $post )
+    <div class="card">
+        {{-- title --}}
+        <h2 class="font-bold text-xl">{{ $post->title }}</h2>
 
-    @guest
-        <h1>Welcome Guest</h1>
-    @endguest
+        {{-- author and date --}}
+        <div class="text-xs font-light mb-4">
+            <span>Posted {{ $post->created_at->diffForHumans() }} by</span>
+            <a href="" class="text-blue-500 font-medium">USERNAME</a>
+
+        </div>
+        {{-- body --}}
+        <div class="text-sm">
+            <p>{{ Str::words($post->body, 15) }}</p>
+        </div>
+    </div>
+    @endforeach
+
+</div>
+<div>
+    {{ $posts->links() }}
+</div>
 </x-layout>
